5bfe8b361bf03119c31a5155924916132da9c8a7
sungmin <chos1909@gmail.com>
2024-06-17 00:22:49 +0900

    비디오 및 이미지 css 수정

--- src/app/intro/_components/Intro.tsx
+++ src/app/intro/_components/Intro.tsx
@@ -1,10 +1,19 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
 import styles from './intro.module.css';
+
 export default function Intro() {
   return (
     <Fragment>
-      <div className={styles.intro}></div>
-      <div className={styles.intro2}></div>
+      <div className={styles.intro}>
+        <video autoPlay muted loop className={styles.videoBackground}>
+          <source src="/coverr-rays-of-the-sun-shining-through-a-leaf-3991-1080p.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className={styles.title}>
+          우리는 고객의 목표에 맞추어 최적의 솔루션을 제공합니다.
+        </div>
+      </div>
+      {/* <div className={styles.intro2}></div> */}
     </Fragment>
-  )
+  );
 }
