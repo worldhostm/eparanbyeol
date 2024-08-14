4d7b298fca0deae36ee5fbd9d07e49c51948d103
SungminCho <chos1909@gmail.com>
2024-08-14 09:06:53 +0900

    배포 설정2

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true, // 페이지가 끝나는 슬래시를 추가 (필요에 따라 설정)
-  output: 'standalone', // standalone 옵션 사용
-  images: {
-    unoptimized: true, // 이미지 최적화를 비활성화 (GitHub Pages에서 필요)
-    }
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
